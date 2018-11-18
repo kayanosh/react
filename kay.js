3e4794e9ad8edef3d77707e0fbf16cbf911e47e8
kayanosh <kaykoh50@gmail.com>
2018-11-17 20:32:52 -0500

    update from last night

--- kay.js
+++ kay.js
@@ -1,2 +1,12 @@
-const myName = 'kayanosh'
+let myName = 'kayanosh'
+console.log(myName)
+
+myName = 'malik'
+console.log(myName) // let is a variable, and can give lots of outputs
+
+// arrow functions
+const printMyName = ()=> {
+    console.log('max')
+}
+
 
